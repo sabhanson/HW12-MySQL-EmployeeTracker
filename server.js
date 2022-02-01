eb4667bf315316013b851d86f9265b671ef407f6
Sabrina Hanson <sabhanson7@gmail.com>
2022-01-31 19:14:52 -0800

    view all departments, view roles, view employees, and add department all work as expected

--- server.js
+++ server.js
@@ -31,7 +31,10 @@
 // WHEN I choose to update an employee role THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 //TODO:
 
+const { star } = require("cli-spinners");
 const express = require("express");
+const inquirer = require('inquirer');
+const { endsWith } = require("lodash");
 const app = express();
 const mysql2 = require("mysql2");
 const PORT = process.env.PORT || 3001;
@@ -39,41 +42,114 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+const database = mysql2.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "employee_db",
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
+
+viewDepartments = () => {
+    database.query('SELECT * FROM departments',(err, data) => {
+        err ? console.error : console.table(data)
+        startMenu();
+    })
+}
+
+viewRoles = () => {
+    database.query('SELECT * FROM roles',(err, data) => {
+        err ? console.error : console.table(data)
+        startMenu();
+    })
+}
+
+viewEmployees = () => {
+    database.query('SELECT * FROM employees',(err, data) => {
+        err ? console.error : console.table(data)
+        startMenu();
+    })
+}
+
+addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "what is the department name?",
+            name: "newDepartment",
+        }
+    ])
+    .then((ans) => {
+        const newDepart = ans.newDepartment;
+        database.query('INSERT INTO departments (department_name) VALUES (?);', newDepart, (err,data) => {err ? console.error : console.log()
+        startMenu();
+        })
+    })
+}
+
+addRole = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "what is the name of the new role?",
+            name: "newRole"
+        }
+    ])
+    .then((ans) => )
+}
+addEmployee = () => {}
+updateEmployee = () => {}
+quit = () => {
+    database.end();
+}
+
+startMenu = () => {
 inquirer.prompt([
     {
       type: "list",
       message: "What would you like to do?",
       name: "startMenu",
-      choices: ["View all departments","View all roles","View all employees", 'Add a department',"Add a role","Add an employee","Update an employee role"]
+      choices: ["View all departments","View all roles","View all employees", 'Add a department',"Add a role","Add an employee","Update an employee role","Quit"]
     },
 ])
 .then((ans) => {
-    switch (ans) {
+    switch (ans.startMenu) {
         case "View all departments":
-            console.log("viewing all departments")
+            viewDepartments();
             break;
-        case "View all departments":
-            console.log("viewing all departments")
+        case "View all roles":
+            viewRoles();
             break;
         case "View all employees":
-            console.log("viewing all employees")
+            viewEmployees();
             break;
+        case "Add a department":
+            addDepartment();
+            break;
+        case "Add a role":
+            addRole();
+            break;
+        case "Add an employee":
+            addEmployee();
+            break;
+        case "Update an employee":
+            updateEmployee();
+            break;
+        case "Quit":
+            quit();
+
         default:
+            break;
     }
-})
+})}
 ;
 
+startMenu();
 
-const database = mysql2.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "employee_db",
-  },
-  console.log(`Connected to the employee_db database.`)
-);
 
-database.query("SELECT * FROM students", function (err, results) {
-  console.log(results);
-});
+// database.query("SELECT * FROM students", function (err, results) {
+//   console.log(results);
+// });
